482bf236dd155500f80d4bc6ea5d127b86521418
SEEMULLERJ_INFO <SEEMULLERJ_INFO@10.134.99.31>
2017-02-27 13:26:28 +0100

    Set the DB ip for every client

--- QQC/config/db.php
+++ QQC/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=10.134.99.31;dbname=qcc_db',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'qqc_mysql',
+    'password' => 'SuperQQC',
     'charset' => 'utf8',
 ];
